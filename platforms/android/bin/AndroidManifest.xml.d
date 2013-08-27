cffa8806b1173f504aedc910a1d26b6cb9bda03d
Roberto Rojas <rrojas@chariotsolutions.com>
2013-08-27 13:19:27 -0400

    Added missing files.

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/robertorojas/projects/chariot/training/phoneGap3/phonegap3-tutorial-firstapp/platforms/android/bin/AndroidManifest.xml : \
-/Users/robertorojas/projects/chariot/training/phoneGap3/phonegap3-tutorial-firstapp/platforms/android/AndroidManifest.xml \
--- platforms/android/gen/com/chariotsolutions/phonegap3/firstapp/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.chariotsolutions.phonegap3.firstapp;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
